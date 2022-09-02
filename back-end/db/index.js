7745f04bc12dfa8c74f0c63021a0295c50e78288
Rajesh-Reel <rajreel2@hotmail.com>
2022-09-02 11:23:18 +0100

    basic template for back end and slight plan

---
+++ back-end/db/index.js
@@ -0,0 +1,13 @@
+//
+import pg from "pg";
+import "dotenv/config";
+export const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export function query(text, params, callback) {
+  return pool.query(text, params, callback);
+}
---
+++ back-end/db/scripts/createTable.js
@@ -0,0 +1,2 @@
+//
+import pool from "../index.js";
